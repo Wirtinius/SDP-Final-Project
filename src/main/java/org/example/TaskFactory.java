a22182c080b4787bcb4a9f801cd71c18fe548986
Wirtinius <115718269+Wirtinius@users.noreply.github.com>
2023-11-19 20:01:30 +0600

    Reload base

--- src/main/java/org/example/TaskFactory.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-// Фабрика для создания тасков
-public class TaskFactory {
-    public static Task createTask(String name, String description, int priority, String deadline) {
-        return new Task(name, description, priority, deadline);
-    }
-}
